ef5b41c27c7134fe99fde860c5ed5ba41c7fd58f
shuhei123-baku <shuhei123@gmail.com>
2026-07-20 02:29:55 +0900

    Add files via upload

---
+++ formspree-config.js
@@ -0,0 +1,5 @@
+// Formspreeで発行されたURLを、下の仮URLと置き換えてください。
+// 送信先メールアドレスはここには書かず、Formspree管理画面だけで設定します。
+window.CORD_FINDER_CONFIG = {
+  formspreeEndpoint: "https://formspree.io/f/REPLACE_WITH_YOUR_FORM_ID",
+};
